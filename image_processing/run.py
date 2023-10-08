764125d0669b54d12bfce8cf7c263e9de35f96b8
wiseman88 <wiseadam02@gmail.com>
2023-10-08 20:48:40 +0200

    feat(run): add run.py file
    Add run.py file, responsible for running
    code to download, save and optimize images
    for each product provided in table of .csv file

---
+++ image_processing/run.py
@@ -0,0 +1,12 @@
+from image_processing.csv_parser import CSVParser
+from image_processing.image_saver import ImageSaver
+
+file_path = '../data/output.csv'
+
+csv_parser = CSVParser(file_path)
+csv_parser.get_images_url('base_image', 'additional_images')
+
+for index, row in csv_parser.csv_rows():
+    name, images, folder_to_save_images = CSVParser.process_csv_row(row)
+
+    ImageSaver.save_images_to_folder(name, images, folder_to_save_images)
