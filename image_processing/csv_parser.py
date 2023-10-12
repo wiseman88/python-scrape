6d624afc012f1b3bbbef1fce28b70d48ec009d37
wiseman88 <wiseadam02@gmail.com>
2023-10-12 17:20:00 +0200

    refactor(csv_parser): rename get_images_url
    Rename function get_images_url to appropriate name
    create_images_column. Function is responsible for
    merging together urls from base_image column and
    additional_images column.

--- image_processing/csv_parser.py
+++ image_processing/csv_parser.py
@@ -5,7 +5,7 @@ class CSVParser:
     def __init__(self, file_path):
         self.df = pd.read_csv(file_path)
 
-    def get_images_url(self, base_image_col, additional_images_col):
+    def create_images_column(self, base_image_col, additional_images_col):
         self.df['images'] = self.df[base_image_col] + ',' + self.df[additional_images_col]
         return self.df['images']
 
--- image_processing/run.py
+++ image_processing/run.py
@@ -4,7 +4,7 @@ from image_processing.image_saver import ImageSaver
 file_path = '../data/output.csv'
 
 csv_parser = CSVParser(file_path)
-csv_parser.get_images_url('base_image', 'additional_images')
+csv_parser.create_images_column('base_image', 'additional_images')
 
 for index, row in csv_parser.csv_rows():
     name, images, folder_to_save_images = CSVParser.process_csv_row(row)
