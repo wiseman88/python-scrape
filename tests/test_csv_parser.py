445ac9083cd3c3a87ce2e8710379e895420f4093
wiseman88 <wiseadam02@gmail.com>
2023-10-13 18:41:40 +0200

    test(csv_parser): Add csv_parser tests
    Add test for csv_parser class:
    - test_csv_rows
    - test_process_images_row
    - test_process_csv_row

--- tests/test_csv_parser.py
+++ tests/test_csv_parser.py
@@ -19,6 +19,21 @@ class TestCSVParser(unittest.TestCase):
         expected_output = pd.Series(data=['img1.jpg,img2.jpg,img3.jpg', 'img4.jpg,img5.jpg,img6.jpg'], name='images')
         pd.testing.assert_series_equal(self.parser.df['images'], expected_output)
 
+    def test_csv_rows(self):
+        rows = list(self.parser.csv_rows())
+        self.assertEqual(len(rows), 2)
+        self.assertEqual(rows[0][1]['sku'], 1)
+
+    def test_process_images_row(self):
+        images_str = 'img1.jpg,img2.jpg,img3.jpg'
+        expected_output = ['img1.jpg', 'img2.jpg', 'img3.jpg']
+        self.assertEqual(CSVParser.process_images_row(images_str), expected_output)
+
+    def test_process_csv_row(self):
+        sample_row = pd.Series({'sku': '1', 'name': 'Product 1', 'images': 'img1.jpg,img2.jpg,img3.jpg'})
+        expected_output = ('Product 1', ['img1.jpg', 'img2.jpg', 'img3.jpg'], '../data/output_images/1')
+        self.assertEqual(CSVParser.process_csv_row(sample_row), expected_output)
+
 
 if __name__ == '__main__':
     unittest.main()
