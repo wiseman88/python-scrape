6a8979e7fa4d8897e64d7c9b1d61c37af0755d89
wiseman88 <wiseadam02@gmail.com>
2023-10-21 09:44:02 +0200

    test(image_optimizer): add test for image optimizer
    Add test for image_optimizer
    - test if image resize successfully
    - test if image is in jpeg format

---
+++ tests/test_image_optimizer.py
@@ -0,0 +1,27 @@
+import unittest
+from io import BytesIO
+
+from PIL import Image
+from src.image_processing.image_optimizer import ImageOptimizer
+
+
+class TestImageOptimizer(unittest.TestCase):
+    def test_resize_image_successfully(self):
+        test_image = Image.new('RGB', (1500, 1600))
+
+        resized_image = ImageOptimizer.resize_image(test_image, max_width=1000, max_height=1000)
+
+        self.assertLessEqual(resized_image.width, 1000)
+        self.assertLessEqual(resized_image.height, 1000)
+
+    def test_jpeg_format(self):
+        image_data = BytesIO()
+        Image.new("RGB", (100, 100)).save(image_data, format="JPEG")
+        image_data.seek(0)
+
+        image = Image.open(image_data)
+        self.assertTrue(ImageOptimizer.check_image_format(image))
+
+
+if __name__ == '__main__':
+    unittest.main()
