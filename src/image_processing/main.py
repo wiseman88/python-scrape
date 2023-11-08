f72f94b9820dd6c0ff74cb7c135cd723575204a9
wiseman88 <wiseadam02@gmail.com>
2023-11-08 18:41:13 +0100

    refactor(image_processing): add main function
    Create main with the code of main.py for better readability
    of code.

--- src/image_processing/main.py
+++ src/image_processing/main.py
@@ -1,12 +1,16 @@
 from src.image_processing.csv_parser import CSVParser
 from src.image_processing.image_saver import ImageSaver
 
-file_path = '../data/output.csv'
 
-csv_parser = CSVParser(file_path)
-csv_parser.create_images_column('base_image', 'additional_images')
+def main(f_path):
+    csv_parser = CSVParser(f_path)
+    csv_parser.create_images_column('base_image', 'additional_images')
 
-for index, row in csv_parser.csv_rows():
-    name, images, folder_to_save_images = CSVParser.process_csv_row(row)
+    for index, row in csv_parser.csv_rows():
+        name, images, folder_to_save_images = CSVParser.process_csv_row(row)
+        ImageSaver.save_images_to_folder(name, images, folder_to_save_images)
 
-    ImageSaver.save_images_to_folder(name, images, folder_to_save_images)
+
+if __name__ == "__main__":
+    file_path = '../data/output.csv'
+    main(file_path)
